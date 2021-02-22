19010bd1b14b00d9f32bdbd67c12c9c3a11b3c5f
El Stevens <eleanorkwstevens@gmail.com>
2021-02-22 15:10:35 +0000

    fixed google maps issue - can now display multiple on one page. also started working on animating navbar scroll animation.

--- app/js/dist/app.js
+++ app/js/dist/app.js
@@ -1,23 +1,23 @@
-function californiaMap() {
-    var options = {
+function initMap() {
+    var caliOptions = {
         zoom: 11,
         center: { lat: 33.170700, lng: -117.242490 }
     };
-    var map = new google.maps.Map(document.getElementById('cali-map'), options);
-    var marker = new google.maps.Marker({
-        position: { lat: 33.170700, lng: -117.242490 },
-        map: map
-    });
-}
-
-function ukMap() {
-    var options = {
+    var ukOptions = {
         zoom: 11,
         center: { lat: 51.505699, lng: -2.551830 }
     };
-    var map = new google.maps.Map(document.getElementById('uk-map'), options);
+    var caliMap = new google.maps.Map(document.getElementById('cali-map'), caliOptions);
+    var ukMap = new google.maps.Map(document.getElementById('uk-map'), ukOptions);
     var marker = new google.maps.Marker({
+        position: { lat: 33.170700, lng: -117.242490 },
+        map: caliMap
+    });
+    var marker2 = new google.maps.Marker({
         position: { lat: 51.505699, lng: -2.551830 },
-        map: map
+        map: ukMap
     });
 }
+window.addEventListener('scroll', function (e) {
+    document.querySelector('nav').classList.toggle('scrolled');
+});
