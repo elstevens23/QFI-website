1d9b7d09fc70a1cf7e4a6b30fe26ba70af0a2394
El Stevens <eleanorkwstevens@gmail.com>
2021-05-01 14:42:19 +0100

    finished the basic structure and content of the website. now need to finish the applications page (Paul wants to change structure) and support page (has no content currently). also now need to start focusing on design and animations, improving the UI and UX

--- app/js/app.ts
+++ app/js/app.ts
@@ -1,25 +1,30 @@
-// import * as $ from 'jquery'
-
-function initMap():void {
+function caliMap():void {
     const caliOptions = {
         zoom: 11,
         center: { lat: 33.170700, lng: -117.242490 }
     };
-    const ukOptions = {
-        zoom: 11,
-        center: { lat: 51.505699, lng: -2.551830 }
-    };
     const caliMap = new google.maps.Map(document.getElementById('cali-map'), caliOptions);
-    const ukMap = new google.maps.Map(document.getElementById('uk-map'), ukOptions);
     const marker = new google.maps.Marker({
         position: { lat: 33.170700, lng: -117.242490 },
         map: caliMap
     });
+}
+
+caliMap();
+
+function ukMap():void {
+    const ukOptions = {
+        zoom: 11,
+        center: { lat: 51.505699, lng: -2.551830 }
+    };
+    const ukMap = new google.maps.Map(document.getElementById('uk-map'), ukOptions);
     const marker2 = new google.maps.Marker({
         position: { lat: 51.505699, lng: -2.551830 },
         map: ukMap
     });
 }
+
+ukMap();
 
 window.addEventListener('scroll', e => {
     if (window.scrollY > window.innerHeight) {
@@ -29,79 +34,3 @@ window.addEventListener('scroll', e => {
     }
 })
 
-
-// document.querySelector(document).ready(function(){
-//     var $window = document.querySelector(window);
-//     document.querySelector('section[data-type="background"]').each(function(){
-//         var $bgobj = document.querySelector(this); // assigning the object
-//
-//         document.querySelector(window).scroll(function() {
-//             var yPos = -($window.scrollTop / $bgobj.data('speed'));
-//
-//             // Put together our final background position
-//             var coords = '50% '+ yPos + 'px';
-//
-//             // Move the background
-//             $bgobj.css({ backgroundPosition: coords });
-//         });
-//     });
-// });
-//
-// //menu transition js
-// document.querySelector(document).ready(function(){
-//     document.querySelector(window).scroll(function(){
-//         var scroll = document.querySelector(window).scrollTop;
-//         if (scroll > 0) {
-//             document.querySelector(".navbar").classList.add("navbar-scroll");
-//         }
-//         else{
-//             document.querySelector(".navbar").removeClass("navbar-scroll");
-//         }
-//         if (scroll > 200) {
-//             document.querySelector(".navbar").classList.add("bg-primary");
-//         }
-//
-//         else{
-//             document.querySelector(".navbar").removeClass("bg-primary");
-//         }
-//     })
-// })
-
-
-
-// $(document).ready(function(){
-//     var $window = $(window);
-//     $('section[data-type="background"]').each(function(){
-//         var $bgobj = $(this); // assigning the object
-//
-//         $(window).scroll(function() {
-//             var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-//
-//             // Put together our final background position
-//             var coords = '50% '+ yPos + 'px';
-//
-//             // Move the background
-//             $bgobj.css({ backgroundPosition: coords });
-//         });
-//     });
-// });
-//
-// //menu transition js
-// $(document).onload(function(){
-//     $(window).scroll(function(){
-//         var scroll = $(window).scrollTop();
-//         if (scroll > 0) {
-//             $(".navbar").addClass("navbar-scroll");
-//         }
-//         else{
-//             $(".navbar").removeClass("navbar-scroll");
-//         }
-//         if (scroll > 200) {
-//             $(".navbar").addClass("bg-primary");
-//         }
-//
-//         else{
-//             $(".navbar").removeClass("bg-primary");
-//         }
-//     })
-// })
--- app/js/dist/app.js
+++ app/js/dist/app.js
@@ -1,23 +1,27 @@
-function initMap() {
+function caliMap() {
     var caliOptions = {
         zoom: 11,
         center: { lat: 33.170700, lng: -117.242490 }
     };
-    var ukOptions = {
-        zoom: 11,
-        center: { lat: 51.505699, lng: -2.551830 }
-    };
     var caliMap = new google.maps.Map(document.getElementById('cali-map'), caliOptions);
-    var ukMap = new google.maps.Map(document.getElementById('uk-map'), ukOptions);
     var marker = new google.maps.Marker({
         position: { lat: 33.170700, lng: -117.242490 },
         map: caliMap
     });
+}
+caliMap();
+function ukMap() {
+    var ukOptions = {
+        zoom: 11,
+        center: { lat: 51.505699, lng: -2.551830 }
+    };
+    var ukMap = new google.maps.Map(document.getElementById('uk-map'), ukOptions);
     var marker2 = new google.maps.Marker({
         position: { lat: 51.505699, lng: -2.551830 },
         map: ukMap
     });
 }
+ukMap();
 window.addEventListener('scroll', function (e) {
     if (window.scrollY > window.innerHeight) {
         document.querySelector('#header').classList.add('scrolled');
